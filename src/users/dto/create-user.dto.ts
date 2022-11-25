b829a9141fe30134c3229da7be9ea1f38f769e17
he445 <103337053+he445@users.noreply.github.com>
2022-11-25 15:43:32 -0300

    feat: db conection

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @IsString()
+  password: string;
+
+  @ApiProperty()
+  @IsString()
+  cpf: string;
+
+  @ApiProperty()
+  @IsString()
+  role: string;
+}
